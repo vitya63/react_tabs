b8c6756e666fb5c94efccd23a42bc436d1936dba
Viktor Lukovets <nuerio63@gmail.com>
2019-10-29 14:25:39 +0200

    Updates

---
+++ precache-manifest.e69ea1b7277fa1ad0299ce8d4c10b0ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98fcff7ee270bd06b584f662f3a90677",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "05c7bc27b6356c3e1b7e",
+    "url": "/react_tabs/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "a16792bbd021bec6ef7e",
+    "url": "/react_tabs/static/js/2.27899654.chunk.js"
+  },
+  {
+    "revision": "05c7bc27b6356c3e1b7e",
+    "url": "/react_tabs/static/js/main.a2b607c2.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
